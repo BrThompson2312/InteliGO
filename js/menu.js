792aeb5b5c5a64b56b89153e7f11872e69c33357
Bruno <brunogomez2312@gmail.com>
2023-06-27 11:30:27 -0300

    nuevo diseño

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,33 @@
 let inicio = document.querySelector('#inicio');
-let home = document.querySelector('#home');
 let admin = document.querySelector('#administradores');
+let home = document.querySelector('#home');
+
+let opciones_hilera = document.getElementsByClassName('opciones-hilera');
 
 let main = document.querySelector('main');
 
-inicio.style.display = "none"
+inicio.style.display = "block"
+opciones_hilera[0].style.backgroundColor = "#427C7C";
+
+admin.style.display = "none"
 
 function opcion_menu( parametro ) {
-    switch(parametro) {
+    switch( parametro ) {
         case 'home':
-            inicio.style.display = "unset";
+            for (let i = 0; i < opciones_hilera.length; i++) {
+                opciones_hilera[i].style.backgroundColor = "#282828";
+            }
+            opciones_hilera[0].style.backgroundColor = "#427C7C";
+            inicio.style.display = "block";
             admin.style.display = "none";
         break;
         case 'administradores':
+            for (let i = 0; i < opciones_hilera.length; i++) {
+                opciones_hilera[i].style.backgroundColor = "#282828";
+            }
+            opciones_hilera[1].style.backgroundColor = "#427C7C";
             inicio.style.display = "none";
-            admin.style.display = "unset";
+            admin.style.display = "block";
         break;
     }
 }
--- menu.php
+++ menu.php
@@ -25,22 +25,29 @@
             <h1 class="inteligo"> INTELI<span>GO</span></h1>
         </section>
         <section id="user">
-            <i class="fa-solid fa-user style-user"></i>
-            <div class="user-categories">
-                <?php
-                    if ($tipoUsuario == 'admin') {
-                        echo'<h3 style="margin: 0"> [Administrador] </h3>';
-                        echo'<h5 style="margin: 5px;"> <!-- Nombre y apellido--> Root Root </h5>';
-                    } else {
-                        echo'<h3 style="margin: 0"> [Operador] </h3>';
-                        echo'<h5 style="margin: 5px;"> <!-- Nombre y apellido--> User User </h5>';
-                    }
-                ?>
+            <a id="logout" href="salir.php"> <i class="fa-solid fa-right-from-bracket"></i> </a>
+            <div class="info-user">
+                <i class="fa-solid fa-user style-user"></i>
+                <div>
+                    <?php
+                        if ($tipoUsuario == 'admin') {
+                            ?>
+                                <h3 style="margin: 0"> [Administrador] </h3>
+                                <h5 style="margin: 5px;"> <!-- Nombre y apellido--> Root Root </h5>
+                            <?php
+                        } else {
+                            ?>
+                                <h3 style="margin: 0"> [Operador] </h3>
+                                <h5 style="margin: 5px;"> <!-- Nombre y apellido--> User User </h5>
+                            <?php 
+                        } ?>
+                </div>
             </div>
         </section>
         <section id="opciones">
             <div class="conjunto-opciones">
                 <div class="opciones-hilera" id="home" onclick="opcion_menu('home')"> Home </div>
+
                 <?php
                     // print_r($_SESSION);
                     if($tipoUsuario=='admin') {
@@ -60,7 +67,6 @@
                 <div class="opciones-hilera" onclick="opcion_menu('historial-de-movimiento')"> Historial de movimiento </div>
                 <div class="opciones-hilera" onclick="opcion_menu('gastos-de-mantenimiento')"> Gastos de mantenimiento </div>
             </div>
-            <a href="salir.php"> Salir </a>
         </section>
     </nav>
     <main>
@@ -76,7 +82,10 @@
                     }
                 ?>
             </div>
-            <img class="logo-img" src="img/logo.png">
+            <div class="logo-img">
+                <img src="img/logo.png">
+                <img src="img/black-logo.png">
+            </div>
         </section>
 
         <?php
